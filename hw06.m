131ed65787d98c650fc8b88ea4851792d5f0337e
DanielKetterer <daniel.kette@gmail.com>
2019-04-15 13:19:34 -0400

    Finsihed

--- hw06.m
+++ hw06.m
@@ -1,18 +1,49 @@
 % hw06.m          -
 % Daniel Ketterer -
 % MTH 6150        -
-clear all;
+
 %%%Problem 1
-fun = @(x) (x-1)*exp(-x^2);
-a = 10;
-b = 40;
-tol = 1e-05;
+clear all;
+fun = @(x) (x-1).*exp(-x.^2);
+t = linspace(-10,10,2000);
+plot(t,fun(t))
+a = -1;
+b = 0;
+tol = 1e-08;
 kmax = 10000;
 [xmin ,fmin ,iter ] = golden(fun, a, b, tol, kmax)
-clear all;
+
 %%%Problem 2
-Ffun = @(x1,x2) x1^4+x1*x2+(1+x2)^2;
-nmax = 10000;
-%[x,F,iter] = newtonsys(Ffun, Jfun, x0 ,tol, nmax, varargin)
+clear all;
+Ffun = @(x) x(1).^4+x(1)*x(2)+(1+x(2)).^2;
+Jfun = @(x) [4*x(1).^3+x(2);x(1)+2*(1+x(2))];
+Hfun = @(x) [12*x(1).^2, 1; 1, 2];
+nmax = 100000;
+tol = 1e-08;
+x0 =[.75;-1.25];
+[x,F,iter] = newtonsys(Jfun,Hfun, x0 ,tol, nmax)
+Ffun(x)
+x0 =[0;0];
+[x,F,iter] = newtonsys(Jfun,Hfun, x0 ,tol, nmax)
+Ffun(x)
+[X,Y] = meshgrid(-3:0.1:3,-6:6);
+Z = X.^4+X.*Y+(1+Y).^2;
+surf(X,Y,Z)
+
 %%%Problem 3
-%[x,err ,iter ]= descent (fun ,grad ,x0 ,tol ,kmax ,meth ,varargin )
+clear all;
+fun=@(x)100*(x(2)-x(1)^2)^2+(1-x(1))^2;
+grad=@(x) [-400*x(1)*x(2)+400*x(1)^3+2*x(1)-2;200*x(2)-200*x(1)^2]
+x0 =[-1.2;1];
+tol = 1e-08;
+kmax = 10000;
+method = 1;
+Hess =@(x) [-400*x(2)+1200*x(1)^2+2,-400*x(1);-400*x(1),200];
+H=Hess(x0);
+[x,err ,iter ]= descent (fun ,grad ,x0 ,tol ,kmax ,method,Hess )
+method = 2;
+[x,err ,iter ]= descent (fun ,grad ,x0 ,tol ,kmax ,method,H )
+method = 3;
+[x,err ,iter ]= descent (fun ,grad ,x0 ,tol ,kmax ,method )
+method = 42;
+[x,err ,iter ]= descent (fun ,grad ,x0 ,tol ,kmax ,method )
